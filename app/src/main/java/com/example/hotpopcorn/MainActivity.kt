e3f201b6b193f80376e81524a837d4da85f10a6c
Kamil Matula <43967269+MKamil99@users.noreply.github.com>
2021-04-27 00:10:46 +0200

    Added basic navigation
    Added navigation with three tabs: Home, Browse and Spotted.

--- app/src/main/java/com/example/hotpopcorn/MainActivity.kt
+++ app/src/main/java/com/example/hotpopcorn/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.hotpopcorn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpNavigation()
+    }
+
+    // Setting up navigation that can be seen at the bottom of the screen:
+    private fun setUpNavigation() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        NavigationUI.setupWithNavController(btm_nav, navHostFragment.navController)
     }
 }
